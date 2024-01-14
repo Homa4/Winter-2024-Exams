93d711862f13dba807f95c24c50c1f026bc59dd6
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 23:38:04 +0200

    Update sum.js

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -3,10 +3,12 @@
 //2.create new 
 //3.create arrow function
 //4.create variable (sumOfNumbers) which will get result
+//5.take array (arr) using method chain call (.filter()) array from to get array of numbers then (.reduce()) to sum all numbers from an array and get result 
 
 const counterOfArrayNumbers = (arr) => {
-  let let sumOfNumbers;
+  let sumOfNumbers = arr.filter(elem => typeof elem === 'number').reduce((accum, elem) => accum + elem, 0);
+  return sumOfNumbers;
 }
 
 
-module.exports = sum;
+module.exports = counterOfArrayNumbers;
