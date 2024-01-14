bc7970fc36fe0825f4444d29603f71158d454049
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 22:04:26 +0200

    Update month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,6 +1,6 @@
 // Get month number
-
-Months = [
+//1. rename function and variables also 
+const arrMonths = [
   'jan',
   'feb',
   'mar',
@@ -15,10 +15,10 @@ Months = [
   'dec',
 ];
 
-Month = (s) => {
-  l = Months.length;
-  for (let i = 0; i < l; i++) {
-    if (s.toLowerCase().startsWith(Months[i])) return i + 1;
+const getMonthNumber = (monthNameStr) => {
+  const lengthOfArrMonth = arrMonths.length;
+  for (let i = 0; i < lengthOfArrMonth; i++) {
+    if (monthNameStr.toLowerCase().startsWith(arrMonths[i])) return i + 1;
   }
   return -1;
 };
