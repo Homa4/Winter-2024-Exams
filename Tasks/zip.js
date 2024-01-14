6c463a82efe6912174aef17e53bb6dad67bb66e3
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-15 00:45:49 +0200

    Update zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,6 +1,7 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 //1. change function name and variables name
-//2.change loop for..of on loop for
+//2.change loop (for..of) on loop (for)
+//3.using (for) we can delete (let j) using variable (i) instead
 const zipTwoArr= function (arr1 = [], arr2 = []) {
 
   for(let i = 0; i<arr2.length; i++){
@@ -11,7 +12,9 @@ const zipTwoArr= function (arr1 = [], arr2 = []) {
       arr2[i] = arrCell;
     }
 
-    if (arrCell[0] == undefined) arr2.length -= 1
+    if (arrCell[0] == undefined){
+      arr2.length -= 1
+    }
   }
   return arr2;
 };
