df1667df07b1f13cdbdd17b493aabd178c5429c8
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 15:44:19 +0200

    1.change name of function and veriables

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,18 +1,18 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-const zip = function (a = [], b = []) {
+const zipTwoArr = function (arr1 = [], arr2 = []) {
   let i = 0;
-  j = 0;
+  let j = 0;
   for (x of b) {
-    CELL = [a[i++], x];
+    let arrCell = [a[i++], x];
     if (i < j) {
       delete a[i++];
     } else {
-      (() => (b[j++] = CELL))();
+      (() => (b[j++] = arrCell))();
     }
-    if (CELL[0] == undefined) b.length -= 1
+    if (arrCell[0] == undefined) b.length -= 1
   }
   return b;
 };
 
-module.exports = zip;
+module.exports = zipTwoArr;
