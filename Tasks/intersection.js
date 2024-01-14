5c68b532989d18bdad2e7dbf46ee967f58d18af4
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 21:50:32 +0200

    Update intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,15 +1,14 @@
 // Find an intersection of two dictionaries
 // 1.delete all unnecessary brackets
 //2.rename name of function and variables
+//3.change logic , delete else and change if condition and execution unit
 function intersectionOfTwoDictionaries(firstObject,secondObject) {
   const keys = Object.keys(firstObject);
 
   for (const elem of keys) {
-    if (firstObject[elem] === secondObject[elem]) {
-      secondObject[elem] = firstObject[elem];
-    } else {
+    if (firstObject[elem] != secondObject[elem]) {
       delete firstObject[elem];
-    }
+    } 
   }
   return firstObject;
 };
