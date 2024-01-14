7e462f5faa7dad6d4389d69aab1089ca67c9f49d
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-15 00:42:47 +0200

    Update zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,16 +1,17 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 //1. change function name and variables name
+//2.change loop for..of on loop for
 const zipTwoArr= function (arr1 = [], arr2 = []) {
-  let i = 0;
-  let j = 0;
-  for (const elem of arr2) {
-    let arrCell = [arr1[i++], elem];
-    if (i < j) {
-      delete arr1[i++];
+
+  for(let i = 0; i<arr2.length; i++){
+    let arrCell = [arr1[i], arr2[i]];
+    if (i < 0) {
+      delete arr1[i];
     } else {
-      (() => (arr2[j++] = arrCell))();
+      arr2[i] = arrCell;
     }
-    if (arrCell[0] == undefined) b.length -= 1
+
+    if (arrCell[0] == undefined) arr2.length -= 1
   }
   return arr2;
 };
