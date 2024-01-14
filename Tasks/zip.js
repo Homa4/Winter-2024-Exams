48bc569e4f07d822709a582de8f5603dee7a1774
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 16:00:27 +0200

    3. veriable let j = 0 was deleted . Function use let i from loop for() to iterat by arrays

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,19 +1,18 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
 const zipTwoArr = function (arr1 = [], arr2 = []) {
-  let j = 0;
 
   for(let i = 0; i < arr2.length; i++){
     let arrCell = [arr1[i], arr2[j]];
-    if(i < j){
+    if(i < 0){
       delete arr1[i];
     } else {
-      arr2[j++] = arrCell;
+      arr2[i] = arrCell;
     }
 
     if(arrCell[0] === undefined){arr2.length -= 1}
   }
-  return b;
+  return arr2;
 };
 
 module.exports = zipTwoArr;
