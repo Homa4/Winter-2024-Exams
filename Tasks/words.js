0a052697d5e71c200d32254a81625d66f9b6cc65
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-15 00:31:41 +0200

    Update words.js

--- Tasks/words.js
+++ Tasks/words.js
@@ -3,25 +3,10 @@
 //2.delete checking if flag true in (if) condition 
 //3.delete useless (else) in construction (if(){}ele{})
 //4.delete useless (if) in construction (if(){}ele{})
+//5.to improve function we will use methods .delete (let flag) and all loop
 const wordsCounter = function (enterStr) {
   let numberOfWords = 0;
-  let flag = false;
-  for (const elem of enterStr) {
-    if (!flag) {
-      if (elem === ' ') {
-        flag = false;
-      } else {
-        flag = true;
-         numberOfWords++;
-      }
-    } else {
-      if (elem === ' ') {
-        flag = false;
-      } else {
-        flag = true;
-      }
-    }
-  }
+  
   return  numberOfWords;
 };
 
