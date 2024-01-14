2a224892ce1a682080061bdfb820250831c3dd13
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 23:16:37 +0200

    Update password.js

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,12 +1,13 @@
 // Generate random password
 //1.name of function and variable was changed
 //2.bring (const index) out of the loop
+// write ( key = key + alphabet[index];) as ( key += alphabet[index];)
 const generatePassword = (alphabet, length) => {
   const maxPasswLen = alphabet.length;
   let key = '';
   const index = Math.floor(Math.random() * maxPasswLen);
   for (let i = 0; i < length; i++) {
-    key = key + alphabet[index];
+    key += alphabet[index];
   }
   return key;
 };
