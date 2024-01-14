3362f008bf213462ca3c91f02893b149d8692d21
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 23:27:47 +0200

    Update sample.js

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,8 +1,9 @@
 // Get one random element from an array
 //1.change function name
+//2.delete (arr = arr[Math.floor(Math.random() * arr.length)];) 
+//instead (return arr[Math.floor(Math.random() * arr.length)];)
 const sample = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
+  return arr[Math.floor(Math.random() * arr.length)];  
 };
 
 module.exports = sample;
