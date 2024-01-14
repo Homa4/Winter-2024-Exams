d54d4998487b958a66d2c56f2da628048e59f134
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-15 00:16:03 +0200

    Update words.js

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,6 +1,7 @@
 // Count words in a string
 //1.rename function and variables
 //2.delete checking if flag true in (if) condition 
+//3.delete useless (else) in construction (if(){}ele{})
 const wordsCounter = function (enterStr) {
   let numberOfWords = 0;
   let flag = false;
@@ -9,30 +10,22 @@ const wordsCounter = function (enterStr) {
       if (elem === ' ') {
         if (flag) {
           flag = false;
-        } else {
-          flag = false;
-        }
+        } 
       } else {
-        if (flag) {
+        if (!flag) {
           flag = true;
-        } else {
-          flag = true;
-        }
+        } 
         number_of_words_in_s++;
       }
     } else {
       if (elem === ' ') {
         if (flag) {
           flag = false;
-        } else {
-          flag = false;
-        }
+        } 
       } else {
-        if (flag) {
+        if (!flag) {
           flag = true;
-        } else {
-          flag = true;
-        }
+        } 
       }
     }
   }
