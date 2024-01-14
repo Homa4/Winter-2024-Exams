a90381c209724de2968d04ae4f69fc4a3f9abed9
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 23:19:54 +0200

    Update random.js

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,6 +1,6 @@
 // Generate random integer value in given range
-
-Random = (min, max) => {
+//1.change function name
+const randomGenerator = (min, max) => {
   if (max === undefined) {
     max = min;
     return Math.floor(Math.random() * (max + 1));
@@ -9,4 +9,4 @@ Random = (min, max) => {
   }
 };
 
-module.exports = Random;
+module.exports = randomGenerator;
