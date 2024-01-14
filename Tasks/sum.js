c6e6911b480a8199663f55dd1e303d4f456943ef
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-14 23:33:34 +0200

    Update sum.js

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,8 +2,10 @@
 //1.delete old code
 //2.create new 
 //3.create arrow function
+//4.create variable (sumOfNumbers) which will get result
 
 const counterOfArrayNumbers = (arr) => {
+  let let sumOfNumbers;
 }
 
 
