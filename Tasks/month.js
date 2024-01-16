531bc3c0bb58013fd2859fa4aaa5ad6e67345d45
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-16 18:59:27 +0200

    Update month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,9 +2,10 @@
 //1. rename function and variables also 
 //2.relocate (arrMonth) in function
 //3.delete variable with array length
+//removed array from function
+const arrMonths = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
 
 const getMonthNumber = (monthNameStr) => {
-  const arrMonths = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
   for (let i = 0; i < arrMonths.length; i++) {
     if (monthNameStr.toLowerCase().startsWith(arrMonths[i])) return i + 1;
   }
