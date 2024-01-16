b3cf2a5ed5fcba190b8f472be30d159a820af122
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-16 18:35:16 +0200

    Update intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,15 +2,16 @@
 // 1.delete all unnecessary brackets
 //2.rename name of function and variables
 //3.change logic , delete else and change if condition and execution unit
+//4.change logik in variable (const keys) using method (.filter()) filtered array bu this condition (object_2.hasOwnProperty(key) && object_1[key] === object_2[key])
 function intersectionOfTwoDictionaries(firstObject,secondObject) {
-  const keys = Object.keys(firstObject);
-
+  const keys = Object.keys(firstObject).filter(key => object_2.hasOwnProperty(key) && object_1[key] === object_2[key]);
+  const resultObject = {};
+  
   for (const elem of keys) {
-    if (firstObject[elem] != secondObject[elem]) {
-      delete firstObject[elem];
-    } 
+     resultObject[key] = firstObject[key];
   }
-  return firstObject;
+  
+  return resultObject;
 };
 
 module.exports = intersectionOfTwoDictionaries;
