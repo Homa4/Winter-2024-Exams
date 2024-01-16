d3d67e00725997f16357baeaaae9a2f42115ce9a
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-01-16 18:51:20 +0200

    Update ip.js

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,19 +1,19 @@
 // Split string by the first occurrence of separator
 //1.change name of function and variables
 //2.change type of loop from for..of to for . So (let j) we don`t need animore
+//3. delete else because it useless
 const parseip = (str) => {
   let arr = [];
-  if (str === ''){
-    return;
-  } else {
-    const arrOfIpElements = str.split('.');
-    if (arrOfIpElements.length != 4) return;
+  if (str === '') return; 
+  
+  const arrOfIpElements = str.split('.');
+  if (arrOfIpElements.length != 4) return;
     
-     for (let i = 0; i < arrOfIpElements.length;i++) {
-      arr[i] = parseInt(arrOfIpElements[i]);
-      if (isNaN(arr[i])) return;
-    }
+  for (let i = 0; i < arrOfIpElements.length; i++) {
+    arr[i] = parseInt(arrOfIpElements[i]);
+    if (isNaN(arr[i])) return;
   }
+  
   return arr;
 };
 
